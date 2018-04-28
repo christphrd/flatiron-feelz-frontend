910e3efaaf2206b84abecd2957e538751b586974
github username <diep.christopher@gmail.com>
2018-04-28 16:03:30 -0400

    cleaned up dog spirit on the show page. no more console.log

--- src/components/DogSpiritShow.js
+++ src/components/DogSpiritShow.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 class DogSpiritShow extends React.Component {
   render(){
-    console.log(this.props.clickedUserData)
     return(
       <div>
         <h3>Dog Spirit of the Feelings of the one and only {this.props.clickedUserData.first_name}</h3>
