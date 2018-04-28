65be31dc2f24d297c88c939ae840b0f5612eeb73
github username <diep.christopher@gmail.com>
2018-04-28 12:55:03 -0400

    doing the fetch in the onClick function allows it to render the clicked user information. earlier fetch resolves the issue.

--- src/components/LoggedInContainer.js
+++ src/components/LoggedInContainer.js
@@ -3,24 +3,28 @@ import UsersList from './UsersList';
 import UserHomePage from './UserHomePage';
 import UserShowPage from './UserShowPage';
 
+const usersURL = `http://localhost:3000/api/v1/users/`
+
 class LoggedInContainer extends React.Component {
   state = {
     home: true,
-    clickedUserID: null
+    clickedUserData: null
   }
 
   goToShow = (event) => {
     //Take the id and fetch the user info. then change home state to false so renders userShowPage
-    this.setState({
-      home: false,
-      clickedUserID: event.target.id
-    })
+    fetch(usersURL + event.target.id)
+      .then(res => res.json())
+      .then(json => this.setState({
+        home: false,
+        clickedUserData: json
+      }), () => console.log(this.state))
   }
 
   render(){
     return(
       <div>
-        {this.state.home ? <UserHomePage /> : <UserShowPage clickedUserID={this.state.clickedUserID}/>}
+        {this.state.home ? <UserHomePage /> : <UserShowPage clickedUserData={this.state.clickedUserData}/>}
         <UsersList goToShow={this.goToShow} />
       </div>
     )
