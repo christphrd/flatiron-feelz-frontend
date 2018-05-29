a70177fc8d87a710d3d3b3f102ea011fa9ca66eb
github username <diep.christopher@gmail.com>
2018-05-29 00:20:39 -0400

    secure websockets

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,7 @@ export const baseURL = `https://floating-wildwood-28213.herokuapp.com/`
 // export const API_ROOT = 'http://192.168.1.8:3000';
 // export const API_WS_ROOT = 'ws://192.168.1.8:3000/cable';
 export const API_ROOT = `https://floating-wildwood-28213.herokuapp.com`;
-export const API_WS_ROOT = 'ws://floating-wildwood-28213.herokuapp.com/cable';
+export const API_WS_ROOT = `wss://floating-wildwood-28213.herokuapp.com/cable`;
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
