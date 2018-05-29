025a95aaba15367c2376ba7348c7a79dda7924ba
github username <diep.christopher@gmail.com>
2018-05-29 00:13:57 -0400

    remove ajax from index.html, update constants baseurl, let user know face analysis unavailable

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,14 @@
 //variables for chatroom
 
-export const baseURL = `http://localhost:3000/`
+export const baseURL = `https://floating-wildwood-28213.herokuapp.com/`
+// export const baseURL = `http://localhost:3000/`
 // export const baseURL = `http://192.168.1.8:3000/`;
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+// export const API_ROOT = 'http://localhost:3000';
+// export const API_WS_ROOT = 'ws://localhost:3000/cable';
 // export const API_ROOT = 'http://192.168.1.8:3000';
 // export const API_WS_ROOT = 'ws://192.168.1.8:3000/cable';
-// export const API_ROOT = `https://floating-wildwood-28213.herokuapp.com`;
-// export const API_WS_ROOT = 'ws://floating-wildwood-28213.herokuapp.com/cable';
+export const API_ROOT = `https://floating-wildwood-28213.herokuapp.com`;
+export const API_WS_ROOT = 'ws://floating-wildwood-28213.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
